64535a2e3a05f84993a4c417f9e2494079a2c290
MasterHoracio <horacio.cpp@gmail.com>
2017-05-15 22:29:13 -0500

    Add files via upload

--- Paying up.cpp
+++ Paying up.cpp
@@ -1,34 +1,27 @@
 #include <cstdio>
-#include <cmath>
+
+#define MAXN 22
 
 using namespace std;
 
-void solve(int n, int m, int *subset){
-	int sum;
-	for(int i = 1; i < pow(2.0,n); i++){
-		sum = 0;
-		for(int j = 0; j < n; j++)
-			if(i&(1<<j))
-				sum += subset[j];
-		if(sum == m){
-			printf("Yes\n");
-			return;
-		}
-	}
-	printf("No\n");
+bool solve(int set[], int start, int end, int goal, int sum){
+	if(sum == goal)
+		return true;
+	if(sum > goal || start >= end)
+		return false;
+	return solve(set, start + 1, end, goal, sum) || solve(set, start + 1, end, goal, sum + set[start]);
 }
 
 int main(){
-	int tc, n, m, subset[25];
-
+	int tc, n, m, set[MAXN];
+	
 	scanf("%d",&tc);
-
+	
 	while(tc--){
 		scanf("%d %d",&n,&m);
 		for(int i = 0; i < n; i++)
-			scanf("%d",&subset[i]);
-		solve(n, m, subset);
+			scanf("%d",&set[i]);
+		(solve(set, 0, n, m, 0)) ? printf("Yes\n") : printf("No\n");
 	}
-
 	return 0;
 }
